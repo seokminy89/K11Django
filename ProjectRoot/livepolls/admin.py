b5807ccc1cfb1215a094533d21854fe65cf8e8ed
seokminy89 <seokminy89@naver.com>
2022-01-03 18:37:53 +0900

    장고 학습.

---
+++ ProjectRoot/livepolls/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from livepolls.models import Question, Choice
+
+'''
+model.py에서 테이블을 생성하면 관리자모드에 적용하기 위해
+아래와 같이 등록절차가 필요하다.
+'''
+
+admin.site.register(Question)
+admin.site.register(Choice)
+
+
